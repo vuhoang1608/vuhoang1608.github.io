28945ce974741f11607bdd707b41082cfda31c97
Jonathan Tran <31745436+vuhoang1608@users.noreply.github.com>
2019-12-18 09:28:56 -0800

    Add files via upload

---
+++ images/aboutMe.py
@@ -0,0 +1,11 @@
+class AboutMe:
+	def __init__(self):
+		self.name = "JONATHAN TRAN"
+	def getLanguages():
+		return ["Python","C++","Java","PowerShell","JavaScript", "HTML", "CSS"]
+	def getTechnicalTools():
+		return ["Ionic", "Angular", "jQuery", "Node.js","MS SharePoint", "Active Directory","and more..."]
+	def getHobbies():
+		return ["coding", "hiking", "running", "backpacking"]
+
+
